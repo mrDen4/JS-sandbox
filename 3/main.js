a9c686d061bafaa098b14633feb92fa5e829cbc4
Denis Davydov <den.saint.k@gmail.com>
2020-03-25 16:15:22 +0300

    fixed mistake in 3 lesson

--- 3/main.js
+++ 3/main.js
@@ -1,37 +1,34 @@
 const START_RANDOM = 1;
 END_RANDOM = 1000;
-let randomNumber = getRandomIntInclusive(START_RANDOM, END_RANDOM);
+const randomNumber = getRandomIntInclusive(START_RANDOM, END_RANDOM);
 //Функция для проверки на число
 let isNumber = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
-function getUserNumber(n) {
-    let userNumber;
-    while (isNumber(n) == false) {
-        n = prompt('Вы ввели не число, введите пожалуйста еще раз');
-        userNumber = n;
+function getUserNumber() {
+    let userNumber = prompt('Введите число, чтобы угадать число от 1 до 1000!');
 
-        if (n == null) {
-            return n = null;
-        }
+    while (isNumber(userNumber) == false && userNumber != null) {
+        userNumber = prompt('Вы ввели не число, введите пожалуйста еще раз');
     }
-    return n;  
+    return userNumber;  
 };
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-function gameWithRandomNumbers() {
-    let userNumber = prompt('Введите число, чтобы угадать число от 1 до 1000!');
-
+function matchCheckWithRandomNumber() {
+    let userNumber = getUserNumber();
     while (userNumber != randomNumber && userNumber != null) {
-        userNumber = getUserNumber(userNumber);
         if (userNumber < randomNumber && userNumber != null) {
             userNumber = prompt('Вы ввели число меньше загаданного, введите другое число');
         }
         if (userNumber > randomNumber) {
             userNumber = prompt('Вы ввели число больше загаданного, введите другое число');
+        }
+        while (isNumber(userNumber) == false && userNumber != null) {
+            userNumber = prompt('Вы ввели не число, введите пожалуйста еще раз');
         }
     }
     if (userNumber == randomNumber) {
@@ -39,4 +36,4 @@ function gameWithRandomNumbers() {
     }
 };
 
-gameWithRandomNumbers();
+matchCheckWithRandomNumber();
