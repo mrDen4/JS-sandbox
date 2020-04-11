c2771554757a6ce4c0b7eddbe28d48d4b4c5e29d
Dennis <mr.den.saint@yandex.ru>
2020-04-11 14:52:43 +0300

    fixed programm with date

--- 4/1 (Вывод даты и времени)/main.js
+++ 4/1 (Вывод даты и времени)/main.js
@@ -19,18 +19,21 @@ function declOfNum(number, titles) {
   return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ]; 
 
 }
-let date = new Date();
-let day = date.getDate();
-let month = date.getMonth();
-let daysOfTheWeek = [
-  'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'
-];
-let monthOfTheYear = [
-  'января', ' февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'
-];
-let year = date.getFullYear();
-let dayOfTheWeek = date.getDay();
-let hours = date.getHours();  
-let minutes = date.getMinutes();  
-let seconds = date.getSeconds();  
-console.log('Сегодня', day, monthOfTheYear[month], year, 'года', daysOfTheWeek[dayOfTheWeek], hours, declOfNum(hours, ['час', 'часа', 'часов']), minutes, declOfNum(minutes, ['минуты', 'минута', 'минут']), seconds, declOfNum(seconds, ['секунда', 'секунды', 'секунд']));
+
+let showDate = setInterval(function () {
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth();
+  let daysOfTheWeek = [
+    'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'
+  ];
+  let monthOfTheYear = [
+    'января', ' февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'
+  ];
+  let year = date.getFullYear();
+  let dayOfTheWeek = date.getDay();
+  let hours = date.getHours();  
+  let minutes = date.getMinutes();  
+  let seconds = date.getSeconds();  
+  console.log('Сегодня', day, monthOfTheYear[month], year, 'года', daysOfTheWeek[dayOfTheWeek], hours, declOfNum(hours, ['час', 'часа', 'часов']), minutes, declOfNum(minutes, ['минута', 'минуты', 'минут']), seconds, declOfNum(seconds, ['секунда', 'секунды', 'секунд']));
+}, 1000);
