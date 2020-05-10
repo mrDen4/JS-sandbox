4a755fb8df299211655ebfcb4a38a568d4d4eb1c
Dennis <mr.den.saint@yandex.ru>
2020-05-11 01:03:08 +0300

    complete 6-th lesson code

--- 6/main.js
+++ 6/main.js
@@ -4,11 +4,23 @@ let user = {
   lastName: '',
   registrationTime: '',
   requestUser() {
-    this.fullName = prompt('Введите Имя и фамилию');
-    this.firstName = this.fullName.split(' ')
+    while (this.fullName != null) {
+      this.fullName = prompt('Введите Имя и фамилию');
+      if (this.fullName != null) {
+        this.fullName = this.fullName.split(' ');
+        this.firstName = this.fullName[0];
+        this.lastName = this.fullName[1];
+        this.registrationTime = new Date();
+        userList.users.push(this.firstName, this.lastName, this.registrationTime)
+      }
+    }
   }
 };
 let userList = {
-  users: []
+  users: [],
+  getAllUsers() {
+    console.log(this.users);
+  }
 };
 user.requestUser();
+userList.getAllUsers();
